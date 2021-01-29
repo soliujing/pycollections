d3979f6a4a1df7522a086761302d3452b05bb77b
soliujing <46142545+soliujing@users.noreply.github.com>
2021-01-29 17:33:12 +0800

    tried multi thread & process

--- rtx_to_csv.py
+++ rtx_to_csv.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
+# %%
 
-import sys, getopt
+import sys, getopt, time
 import xml.etree.ElementTree as ET
 import pandas as pd
+import concurrent.futures
+import os, threading
+
+def rtxV_to_df(root, ns, i):
+# #use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
+    # start_time = time.time()
+    # # time.sleep(1)
+    # print("%02d start------>"%(i))
+    # print("process : %s    , ---- thread : %s  .." % (os.getpid(), threading.currentThread().ident))
+    rtxRows = root.findall("rtx:data/rtx:r/rtx:v[" + str(i+1) + "]", ns)
+    # df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
+    # print("%02d end  <------ with %s seconds.. " % (i, (time.time() - start_time)))
+    return rtxRows
 
 
 def rtx_convert(input, output):
@@ -18,13 +32,41 @@ def rtx_convert(input, output):
     for rtxHeader in root.findall("rtx:metadata/rtx:columns//rtx:name", ns):
         df[rtxHeader.text]=""
 
-    # use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
+# ### use threadpool executor
+# ### use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
+#     p = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+#     futures = {}
+#     for i in range(0, len(df.columns)):
+#         #print("%02d new thread------>"%(i))
+#         futures[i] = p.submit(rtxV_to_df, root, ns, i)
+#     p.shutdown()
+#     for i in range(0, len(df.columns)):
+#         rtxRows = futures[i].result()
+#         df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
+
+# # #use processpool executor
+# # #use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
+#     p = concurrent.futures.ProcessPoolExecutor(max_workers=4)
+#     futures = {}
+#     for i in range(0, len(df.columns)):
+#         #print("%02d new process------>"%(i))
+#         futures[i] = p.submit(rtxV_to_df, root, ns, i)
+#     p.shutdown()    
+#     for i in range(0, len(df.columns)):
+#         rtxRows = futures[i].result()
+#         df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
+
+
+# #single thread
     for i in range(0, len(df.columns)):
-        rtxRows = root.findall("rtx:data/rtx:r/rtx:v[" + str(i+1) + "]", ns)
+        rtxRows = rtxV_to_df(root, ns, i)
         df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
-    
+        # rtxRows = root.findall("rtx:data/rtx:r/rtx:v[" + str(i+1) + "]", ns)
+        # df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
+
     # convert to csv
     df.to_csv(output, index=False)
+    # print("done!")
 
 def main(argv):
     cmdprompt='python3 rtx_to_csv.py -i <inputfile> -o <outputfile>'
@@ -49,4 +91,9 @@ def main(argv):
         print(cmdprompt)
 
 if __name__ == "__main__":
+    start_time = time.time()
     main(sys.argv[1:])
+    print("--- program ran about --- %s seconds ---" % (time.time() - start_time))
+
+
+# %%

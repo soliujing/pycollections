6c9dd886d4b6433b81ea22b1fc1c42366de3da85
soliujing <46142545+soliujing@users.noreply.github.com>
2021-02-01 18:08:29 +0800

    Add files via upload

--- rtx_to_csv.py
+++ rtx_to_csv.py
@@ -1,29 +1,58 @@
 #!/usr/bin/python
 # %%
 
+import copyreg
+import io
+import itertools
 import sys, getopt, time
 import xml.etree.ElementTree as ET
+# from lxml import etree as ET
 import pandas as pd
 import concurrent.futures
 import os, threading
+from pathos.multiprocessing import ProcessingPool
 
-def rtxV_to_df(root, ns, i):
-# #use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
+ns = {'rtx':'http://schema.redwood.com/report/rtx.xsd'}
+
+def element_tree_unpickler(data):
     # start_time = time.time()
-    # # time.sleep(1)
+    # a = ET.parse(io.BytesIO(data))
+    # print("--- element_tree_unpickler ran about --- %3.6f seconds ---" % (time.time() - start_time))
+    # return a
+    return ET.parse(io.BytesIO(data))
+
+def element_tree_pickler(element):
+    # start_time = time.time()
+    # a = element_tree_unpickler, (ET.tostring(element),)
+    # print("--- element_tree_pickler ran about --- %3.6f seconds ---" % (time.time() - start_time))
+    # return a
+    return element_tree_unpickler, (ET.tostring(element),)
+
+
+copyreg.pickle(ET.Element, element_tree_pickler, element_tree_unpickler)
+
+def rtxV_to_df(root, i):
+# #use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
+    # # # time.sleep(1)
+    # start_time = time.time()
     # print("%02d start------>"%(i))
-    # print("process : %s    , ---- thread : %s  .." % (os.getpid(), threading.currentThread().ident))
+    # # print("process : %s    , ---- thread : %s  .." % (os.getpid(), threading.currentThread().ident))
+    # print(root)
+
     rtxRows = root.findall("rtx:data/rtx:r/rtx:v[" + str(i+1) + "]", ns)
+    rtxValues = pd.Series((rtxV.text for rtxV in rtxRows))
+    # print("column %02d completed !" % (i))
     # df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
-    # print("%02d end  <------ with %s seconds.. " % (i, (time.time() - start_time)))
-    return rtxRows
+    # print("%02d end  <------ with %3.6f seconds.. " % (i, (time.time() - start_time)))
+    # return (i, rtxRows)
+    return (i, rtxValues)
+
 
 
 def rtx_convert(input, output):
     #use pandas to convert
     tree = ET.parse(input)
     root = tree.getroot()
-    ns = {'rtx':'http://schema.redwood.com/report/rtx.xsd'}
 
     #create new data frame
     df:pd.DataFrame = pd.DataFrame()
@@ -32,37 +61,34 @@ def rtx_convert(input, output):
     for rtxHeader in root.findall("rtx:metadata/rtx:columns//rtx:name", ns):
         df[rtxHeader.text]=""
 
-# ### use threadpool executor
-# ### use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
-#     p = concurrent.futures.ThreadPoolExecutor(max_workers=4)
-#     futures = {}
-#     for i in range(0, len(df.columns)):
-#         #print("%02d new thread------>"%(i))
-#         futures[i] = p.submit(rtxV_to_df, root, ns, i)
-#     p.shutdown()
-#     for i in range(0, len(df.columns)):
-#         rtxRows = futures[i].result()
-#         df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
+    columns = len(df.columns)
 
-# # #use processpool executor
-# # #use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
-#     p = concurrent.futures.ProcessPoolExecutor(max_workers=4)
-#     futures = {}
-#     for i in range(0, len(df.columns)):
-#         #print("%02d new process------>"%(i))
-#         futures[i] = p.submit(rtxV_to_df, root, ns, i)
-#     p.shutdown()    
-#     for i in range(0, len(df.columns)):
-#         rtxRows = futures[i].result()
-#         df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
+### use threadpool executor
+### use XPATH for rtx values "/table/data/r/v" undernamespace 'rtx'
+    p = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+    futures = {}
+    for i in range(0, columns):
+        #print("%02d new thread------>"%(i))
+        futures[i] = p.submit(rtxV_to_df, root, i)
+    p.shutdown()
+    for i in range(0, columns):
+        column, rtxRows = futures[i].result()
+        df.iloc[:, column]=rtxRows
+
+# ### user ProcessingPool and pickler & unpickler
+#     res = ProcessingPool(4).map(rtxV_to_df, itertools.repeat(root,columns), range(0, columns))
+#     for column, rtxRows in res:
+#         # print(column)
+#         # print(type(rtxRows))
+#         df.iloc[:, column]=rtxRows
 
 
-# #single thread
-    for i in range(0, len(df.columns)):
-        rtxRows = rtxV_to_df(root, ns, i)
-        df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
-        # rtxRows = root.findall("rtx:data/rtx:r/rtx:v[" + str(i+1) + "]", ns)
-        # df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
+# # #single thread
+#     for i in range(0, columns):
+#         column, rtxRows = rtxV_to_df(root, i)
+#         df.iloc[:, column]=rtxRows
+#         # rtxRows = root.findall("rtx:data/rtx:r/rtx:v[" + str(i+1) + "]", ns)
+#         # df.iloc[:, i]=pd.Series((rtxV.text for rtxV in rtxRows))
 
     # convert to csv
     df.to_csv(output, index=False)
@@ -93,7 +119,7 @@ def main(argv):
 if __name__ == "__main__":
     start_time = time.time()
     main(sys.argv[1:])
-    print("--- program ran about --- %s seconds ---" % (time.time() - start_time))
+    print("--- program ran about --- %3.6f seconds ---" % (time.time() - start_time))
 
 
 # %%
